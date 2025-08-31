05d54f9330d4162bd245a9b9d1c883085646f46b
lachose-jB <jadjahouisso@gmail.com>
2025-08-31 23:37:24 +0200

    updatecommit

--- src/data/team.ts
+++ src/data/team.ts
@@ -6,20 +6,20 @@ export const teamMembers: TeamMember[] = [
     name: 'IBADJI',
     position: 'Directeur Général',
     positionEn: 'CEO',
-    image: '/public/assets/img/badji.png'
+    image: '/assets/img/badji.png'
   },
   {
     id: '2',
     name: 'Marie Dubois',
     position: 'Directrice Développement International',
     positionEn: 'International Development Director',
-    image: '/public/assets/img/file.png'
+    image: '/assets/img/file.png'
   },
   {
     id: '3',
     name: 'LOÏS',
     position: 'Responsable Formation',
     positionEn: 'Training Manager',
-    image: '/public/assets/img/file2.png'
+    image: '/asset/img/file2.png'
   }
 ];
